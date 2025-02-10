2667de7e9d436a116bfb22541b7cac6273363b53
Tsvetomir Belchev <99121376+Owtlan@users.noreply.github.com>
2025-02-10 12:27:23 +0200

    add edit page

---
+++ client/src/components/edit-product/EditProduct.jsx
@@ -0,0 +1,141 @@
+import { getAuth } from "firebase/auth";
+import { doc, getDoc, updateDoc } from "firebase/firestore";
+import { db } from "../../firebase-config";
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+
+
+
+
+export default function EditProduct() {
+
+    const { id } = useParams();
+    const navigate = useNavigate();
+    const [product, setProduct] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState("");
+
+
+    const [name, setName] = useState("");
+    const [price, setPrice] = useState("");
+    const [description, setDescription] = useState("");
+    const [category, setCategory] = useState("");
+
+
+    const auth = getAuth();
+    const currentUser = auth.currentUser
+    console.log(currentUser);
+
+    useEffect(() => {
+        const fetchProduct = async () => {
+
+            if (!currentUser) return;
+
+            try {
+                // da pitam bota za tova
+                const docRef = doc(db, "products", id)
+                const docSnap = await getDoc(docRef)
+
+                if (docSnap.exists()) {
+                    const productData = docSnap.data();
+
+                    if (currentUser && productData.userId === currentUser.uid) {
+                        setProduct(productData)
+                        setName(productData.name)
+                        setPrice(productData.price)
+                        setDescription(productData.description)
+                        setCategory(productData.category)
+                    } else {
+                        setError("Нямате права за редактиране на този продукт.")
+                    }
+                } else {
+                    setError("Продуктът не беше намерен.");
+                }
+            } catch (error) {
+                setError("Грешка при зареждане на продукта.");
+
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchProduct();
+    }, [id, currentUser]);
+
+
+    const handleUpdate = async (e) => {
+        e.preventDefault();
+
+        if (!name || !price || !description || !category) {
+            setError("Моля, попълнете всички полета!")
+            return;
+        }
+
+        try {
+            const productRef = doc(db, "products", id);
+            await updateDoc(productRef, {
+                name,
+                price: Number(price),
+                description,
+                category,
+            });
+            alert("Продуктът е обновен успешно!");
+            navigate(`/details/${id}`);
+        } catch (error) {
+            setError("Грешка при обновяването на продукта.");
+        }
+    };
+
+    if (loading) return <p className="text-center text-lg mt-10">Зареждане...</p>;
+    if (error) return <p className="text-center text-lg mt-10 text-red-500">{error}</p>;
+
+
+    return (
+
+        <div className="flex justify-center items-center min-h-screen bg-gray-100">
+            <div className="bg-white p-8 rounded-lg shadow-lg w-96">
+                <h2 className="text-2xl font-semibold text-center">Редактиране на продукт</h2>
+                {error && <p className="text-red-500 text-center">{error}</p>}
+
+                <form onSubmit={handleUpdate} className="mt-4">
+
+                    <div>
+                        <label className="block mb-1">Име на Продукта</label>
+                        <input type="text" className="w-full p-2 border rounded" value={name} onChange={(e) => setName(e.target.value)} />
+                    </div>
+
+                    <div className="mt-3">
+                        <label className="block mb-1">Цена</label>
+                        <input type="number" className="w-full p-2 border rounded" value={price} onChange={(e) => setPrice(e.target.value)} />
+                    </div>
+
+                    <div className="mt-3">
+                        <label className="block mb-1">Описание</label>
+                        <textarea className="w-full p-2 border rounded" value={description} onChange={(e) => setDescription(e.target.value)} />
+                    </div>
+
+                    <div className="mt-3">
+                        <label className="block mb-1">Категория</label>
+                        <select className="w-full p-2 border rounded" value={category} onChange={(e) => setCategory(e.target.value)}>
+                            <option value="">Изберете категория</option>
+                            <option value="Мъжки Обувки">Мъжки Обувки</option>
+                            <option value="Мъжки Якета">Мъжки Якета</option>
+                            <option value="Женски Якета">Женски Якета</option>
+                            <option value="Дамски Обувки">Дамски Обувки</option>
+                            <option value="Мъжки Тениски">Мъжки Тениски</option>
+                            <option value="Женски Тениски">Женски Тениски</option>
+                        </select>
+                    </div>
+
+                    <button type="submit" className="mt-4 w-full bg-blue-500 text-white p-2 rounded hover:bg-blue-600 transition">
+                        Запази промените
+                    </button>
+
+                </form>
+
+            </div>
+        </div>
+    )
+
+
+}
