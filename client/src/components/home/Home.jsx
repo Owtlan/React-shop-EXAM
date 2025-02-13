c250d7fadcdd0e9c32c872fa62ff190f51229642
Tsvetomir Belchev <99121376+Owtlan@users.noreply.github.com>
2025-02-13 11:16:44 +0200

    update homejsx

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -27,28 +27,15 @@ export default function Home() {
         <>
             <div className="category-selector text-center mt-8">
                 <button onClick={() => setCategory("Мъжки Обувки")} className="bg-blue-500 text-white p-2 m-2 rounded">Мъжки Обувки</button>
-                {/* Останалите бутони... */}
+                <button onClick={() => setCategory("Мъжки Якета")} className="bg-blue-500 text-white p-2 m-2 rounded">Мъжки Якета</button>
+                <button onClick={() => setCategory("Дамски Обувки")} className="bg-blue-500 text-white p-2 m-2 rounded">Дамски Обувки</button>
+                <button onClick={() => setCategory("Женски Якета")} className="bg-blue-500 text-white p-2 m-2 rounded">Женски Якета</button>
+                <button onClick={() => setCategory("Мъжки Тениски")} className="bg-blue-500 text-white p-2 m-2 rounded">Мъжки Тениски</button>
+                <button onClick={() => setCategory("Женски Тениски")} className="bg-blue-500 text-white p-2 m-2 rounded">Женски Тениски</button>
+                <button onClick={() => setCategory("")} className="bg-blue-500 text-white p-2 m-2 rounded">Всички Продукти</button>
             </div>
 
             <Catalog category={category} />
-
-            {/* Списък с потребители и бутон за чат */}
-            <div className="user-chat-list text-center mt-8">
-                <h2 className="text-xl font-bold">Потребители за чат:</h2>
-                <ul>
-                    {users.map((user) => (
-                        <li key={user.uid} className="my-4">
-                            <span>{user.email}</span>
-                            {/* Бутон за чат */}
-                            <Link to={`/chat/${user.uid}`}>
-                                <button className="bg-blue-500 text-white px-4 py-2 ml-4 rounded">
-                                    Чат
-                                </button>
-                            </Link>
-                        </li>
-                    ))}
-                </ul>
-            </div>
         </>
     );
 }
