413d41d96560033e9f7a474adf1cbee8a8be4379
Tsvetomir Belchev <99121376+Owtlan@users.noreply.github.com>
2025-02-14 11:07:58 +0200

    adding UserList.jsx

---
+++ client/src/components/users/UserList.jsx
@@ -0,0 +1,21 @@
+// import { Link } from "react-router-dom";
+
+// export default function UsersList({ users }) {
+//     return (
+//         <div className="p-4">
+//             <h2 className="text-xl font-bold">Списък с потребители</h2>
+//             <ul>
+//                 {users.map((user) => (
+//                     <li key={user.uid} className="flex justify-between items-center p-2 border-b">
+//                         <span>{user.email}</span>
+//                         <Link to={`/chat/${user.uid}`}>
+//                             <button className="bg-blue-500 text-white px-3 py-1 rounded">
+//                                 Чат
+//                             </button>
+//                         </Link>
+//                     </li>
+//                 ))}
+//             </ul>
+//         </div>
+//     );
+// }
