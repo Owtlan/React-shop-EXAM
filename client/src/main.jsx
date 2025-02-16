ce1be1e5e48cefe293651fd473026aafddd8567c
Tsvetomir Belchev <99121376+Owtlan@users.noreply.github.com>
2025-02-16 14:44:04 +0200

    adding cartProvider

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './style.css'
 import { AuthProvider } from './AuthContext/authContext.jsx'
+import { CartProvider } from './context/CartContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   // <StrictMode>
   <AuthProvider>
-    <App />
+    <CartProvider>
+      <App />
+    </CartProvider>
   </AuthProvider>
   /* </StrictMode>, */
 )
