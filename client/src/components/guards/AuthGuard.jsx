6188883cdf729ec9ebabd8d4a0e8e351159cf5f4
Tsvetomir Belchev <99121376+Owtlan@users.noreply.github.com>
2025-02-21 09:28:19 +0200

    adding authGuard

---
+++ client/src/components/guards/AuthGuard.jsx
@@ -0,0 +1,39 @@
+import { onAuthStateChanged } from "firebase/auth";
+import { useEffect, useState } from "react";
+import { Navigate } from "react-router-dom";
+import { auth } from "../../firebase-config";
+
+
+export default function AuthGuard({ children }) {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+            setUser(currentUser);
+            setLoading(false);
+        });
+
+        return () => unsubscribe();
+    }, [])
+
+    if (loading)
+        return (
+
+            <div className="flex-col gap-4 w-full flex items-center justify-center">
+                <div
+                    className="w-20 h-20 border-4 border-transparent text-blue-400 text-4xl animate-spin flex items-center justify-center border-t-blue-400 rounded-full"
+                >
+                    <div
+                        className="w-16 h-16 border-4 border-transparent text-red-400 text-2xl animate-spin flex items-center justify-center border-t-red-400 rounded-full"
+                    ></div>
+                </div>
+            </div>
+
+        )
+
+
+
+    return user ? children : <Navigate to="/login" />;
+}
