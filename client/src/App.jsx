7713cbf8a01b3adf33162d0c3b684b2e40bf6a35
Tsvetomir Belchev <99121376+Owtlan@users.noreply.github.com>
2025-02-22 10:07:09 +0200

    adding authguard and guest guard in my app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,27 +13,37 @@ import EditProduct from './components/edit-product/EditProduct.jsx'
 import Chat from './components/chat/Chat.jsx'
 import UsersPage from './components/usersPage/UsersPage.jsx'
 import Checkout from './components/checkout/Checkout.jsx'
+import AuthGuard from './components/GUards/AuthGuard.jsx'
+import GuestGuard from './components/guards/GuestGuard.jsx'
+import Footer from './components/footer/Footer.jsx'
 
 function App() {
 
   return (
     <>
       <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/register" element={<Register />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/logout' element={<Logout />} />
-          <Route path='/create' element={<CreateProduct />} />
-          <Route path='catalog' element={<Catalog />} />
-          <Route path='/details/:id' element={<Details />} />
-          <Route path='/edit/:id' element={<EditProduct />} />
-          <Route path="/users" element={<UsersPage />} /> {/* Страница с потребители */}
-          <Route path="/Checkout" element={<Checkout />} />
-          <Route path='/chat/:userId' element={<Chat />} />
-        </Routes>
+        <div className="flex flex-col min-h-screen">
+          <Navbar />
+          {/* Това избутва футъра надолу */}
+
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/register" element={<GuestGuard><Register /></GuestGuard>} />
+            <Route path='/login' element={<GuestGuard><Login /></GuestGuard>} />
+            <Route path='/logout' element={<Logout />} />
+            <Route path='/create' element={<CreateProduct />} />
+            <Route path='catalog' element={<Catalog />} />
+            <Route path='/details/:id' element={<Details />} />
+            <Route path='/edit/:id' element={<EditProduct />} />
+            <Route path="/users" element={<UsersPage />} /> {/* Страница с потребители */}
+            <Route path="/checkout" element={<AuthGuard> <Checkout /></AuthGuard>} />
+            <Route path='/chat/:userId' element={<Chat />} />
+          </Routes>
+
+          <Footer />
+        </div>
       </Router>
+
     </>
   )
 }
