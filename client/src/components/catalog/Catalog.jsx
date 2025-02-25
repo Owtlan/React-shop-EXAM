c6369d4781e10f40b17608172e122af57c6079b7
Tsvetomir Belchev <99121376+Owtlan@users.noreply.github.com>
2025-02-25 09:20:41 +0200

    updare my loader in my catalog

--- client/src/components/catalog/Catalog.jsx
+++ client/src/components/catalog/Catalog.jsx
@@ -42,7 +42,17 @@ export default function Catalog({ category }) {
     }, [])
 
     if (loading) {
-        return <p className="text-center text-lg mt-10">Зареждане...</p>;
+        return (
+            <div className="flex items-center justify-center h-screen">
+                <div
+                    className="w-20 h-20 border-4 border-transparent text-blue-400 text-4xl animate-spin flex items-center justify-center border-t-blue-400 rounded-full"
+                >
+                    <div
+                        className="w-16 h-16 border-4 border-transparent text-red-400 text-2xl animate-spin flex items-center justify-center border-t-red-400 rounded-full"
+                    ></div>
+                </div>
+            </div>
+        )
     }
 
     const filterProducts = category
