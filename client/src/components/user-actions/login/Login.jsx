13bc5520808f150d9f0e925ca9da76d77145fb33
Tsvetomir Belchev <99121376+Owtlan@users.noreply.github.com>
2025-02-26 10:44:00 +0200

    remove background in my login jsx

--- client/src/components/user-actions/login/Login.jsx
+++ client/src/components/user-actions/login/Login.jsx
@@ -55,7 +55,7 @@ export default function Login() {
     return (
         <>
 
-            <div className="flex justify-center items-center min-h-screen bg-gray-100">
+            <div className="flex justify-center items-center min-h-screen">
                 <div className="bg-white p-8 rounded-lg shadow-lg w-96">
                     <h2 className="text-2xl font-semibold text-center">Вход</h2>
                     {error && <p className="text-red-500 text-center">{error}</p>}
