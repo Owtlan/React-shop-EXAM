6f6272a919187b8f9ec9125a70a7fce1b29622fb
Tsvetomir Belchev <99121376+Owtlan@users.noreply.github.com>
2025-03-11 10:32:26 +0200

    new like button

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -44,12 +44,56 @@ export default function Home() {
             <div className="pt-15 sm:pt-10 w-2/6 lg:w-1/6 md:w-1/4 sm:w-1/4 p-4 bg-gray-100 min-h-screen">
                 <div className="max-w-xs mx-auto">
                     <h3 className="sm:text-xl text-base font-bold mb-4">Филтриране</h3>
-                    <button
+                    {/* <button
                         className={`w-full py-2 mb-2 rounded ${showLiked ? "bg-blue-500 text-white" : "bg-gray-200 text-black"}`}
                         onClick={() => setShowLiked(!showLiked)}
                     >
                         {showLiked ? "Показване на всички" : "❤️ Само харесаните"}
+                    </button> */}
+
+
+
+                    <button
+                        className="group flex w-fit cursor-pointer items-center gap-2 overflow-hidden border rounded-full border-pink-700 fill-none p-2 px-3 font-extrabold text-pink-500 transition-all active:scale-90 peer-checked:fill-pink-500 peer-checked:hover:text-white"
+                        onClick={() => setShowLiked(!showLiked)}
+                    >
+                        <div className="z-10 transition group-hover:translate-x-4">Само харесаните</div>
+                        <svg
+                            className="size-6 transition duration-500 group-hover:scale-[2800%]"
+                            stroke="currentColor"
+                            strokeWidth="1.5"
+                            viewBox="0 0 24 24"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path
+                                d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z"
+                                strokeLinejoin="round"
+                                strokeLinecap="round"
+                            ></path>
+                        </svg>
                     </button>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
                     {/* тук е проблема не ми филтрира правилно */}
                     <h4 className="font-semibold mt-4 mb-2">Рейтинг</h4>
