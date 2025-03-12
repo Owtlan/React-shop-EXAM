9acc38992577a6e2eb6b3c0c02c2fbd27fdec9bf
Tsvetomir Belchev <99121376+Owtlan@users.noreply.github.com>
2025-03-12 10:33:57 +0200

    change color

--- client/src/components/details/Details.jsx
+++ client/src/components/details/Details.jsx
@@ -53,7 +53,7 @@ export default function Details() {
         return (
             <div className="flex items-center justify-center h-screen">
                 <div
-                    className="w-20 h-20 border-4 border-transparent text-blue-400 text-4xl animate-spin flex items-center justify-center border-t-blue-400 rounded-full"
+                    className="w-20 h-20 border-4 border-transparent text-blue-300 text-4xl animate-spin flex items-center justify-center border-t-blue-400 rounded-full"
                 >
                     <div
                         className="w-16 h-16 border-4 border-transparent text-red-400 text-2xl animate-spin flex items-center justify-center border-t-red-400 rounded-full"
