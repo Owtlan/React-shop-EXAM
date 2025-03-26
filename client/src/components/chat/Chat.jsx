e2a0006ad7b1f12ae32174072b1c2226e8922aa8
Tsvetomir Belchev <99121376+Owtlan@users.noreply.github.com>
2025-03-26 11:20:26 +0200

    use chatPartner state to take chat partner name for my h2

--- client/src/components/chat/Chat.jsx
+++ client/src/components/chat/Chat.jsx
@@ -8,7 +8,29 @@ import { db } from "../../firebase-config";
 export default function Chat() {
     const { userId } = useParams();
     const [chatPartnerId, setChatPartnerId] = useState(null);
+    const [chatPartner, setChatPartner] = useState(null);
     const [loading, setLoading] = useState(true);
+
+
+    useEffect(() => {
+        const fetchUserData = async () => {
+            const userRef = doc(db, "users", userId);
+            const userSnap = await getDoc(userRef)
+
+
+            if (userSnap.exists()) {
+                setChatPartner(userSnap.data());
+            } else {
+                console.error("Потребителят не е намерен!");
+            }
+            setLoading(false);
+        }
+
+        if (userId) {
+            fetchUserData();  // Зареждаме данните само ако имаме userId
+        }
+    }, [userId])
+
 
 
     useEffect(() => {
@@ -48,11 +70,14 @@ export default function Chat() {
         </div>
 
     </div>;
-    if (!chatPartnerId) return <p>Грешка: Потребителят не е намерен.</p>;
+    if (!chatPartner) return <p>Грешка: Потребителят не е намерен.</p>;
 
     return (
-        <div className="max-w-lg mx-auto shadow-md rounded-lg overflow-hidden mt-20">
-            <h2>Чат с {chatPartnerId}</h2>
+        <div className="max-w-lg mx-auto shadow-md rounded-lg overflow-hidden mt-20 bg-white p-4">
+            <h2 className="text-2xl font-semibold text-gray-800 mb-4 relative">
+                Чат с {chatPartner.email}
+                <span className="absolute bottom-0 left-0 w-full h-1 bg-gradient-to-r from-blue-500 to-purple-600 rounded-lg"></span>
+            </h2>
             <ChatMessages userId={chatPartnerId} />
             <ChatInput userId={chatPartnerId} />
         </div>
