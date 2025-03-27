7c0eaa5f5e6485cb4e8af0730ad63795a5db2806
Tsvetomir Belchev <99121376+Owtlan@users.noreply.github.com>
2025-03-27 10:47:07 +0200

    change category name and structure

--- client/src/components/create/CreateProduct.jsx
+++ client/src/components/create/CreateProduct.jsx
@@ -177,12 +177,12 @@ export default function CreateProduct() {
                                 <option value="">Изберете категория</option>
                                 <option value="Мъжки Обувки">Мъжки Обувки</option>
                                 <option value="Мъжки Якета">Мъжки Якета</option>
-                                <option value="Женски Якета">Женски Якета</option>
-                                <option value="Дамски Обувки">Дамски Обувки</option>
-                                <option value="Мъжки Тениски">Мъжки Тениски</option>
-                                <option value="Женски Тениски">Женски Тениски</option>
-                                <option value="Женски Дънки">Женски Дънки</option>
                                 <option value="Мъжки Дънки">Мъжки Дънки</option>
+                                <option value="Мъжки Тениски">Мъжки Тениски</option>
+                                <option value="Женски Обувки">Женски Обувки</option>
+                                <option value="Женски Якета">Женски Якета</option>
+                                <option value="Женски Дънки">Женски Дънки</option>
+                                <option value="Женски Тениски">Женски Тениски</option>
                             </select>
                         </div>
 
