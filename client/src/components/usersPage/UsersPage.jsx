1d1b62e6e68c19503a43680c2191a610604a4c74
Tsvetomir Belchev <99121376+Owtlan@users.noreply.github.com>
2025-04-01 12:31:17 +0300

    update for my userpage responsive design

--- client/src/components/usersPage/UsersPage.jsx
+++ client/src/components/usersPage/UsersPage.jsx
@@ -77,7 +77,7 @@ export default function UsersPage() {
     }
 
     return (
-        <div className="max-w-6xl mx-auto p-6 bg-white shadow-lg rounded-lg mt-60">
+        <div className="max-w-6xl mx-auto p-6 bg-white shadow-lg rounded-lg mt-60 mb-4">
             <h2 className="text-2xl font-bold text-gray-800 text-center mb-6">📜 Списък с потребители</h2>
 
             <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
