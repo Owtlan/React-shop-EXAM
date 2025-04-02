722044cdf48beefc5b0bf28f95744794ca28783c
Tsvetomir Belchev <99121376+Owtlan@users.noreply.github.com>
2025-04-02 12:53:12 +0300

    remove padding

--- client/src/components/core/footer/Footer.jsx
+++ client/src/components/core/footer/Footer.jsx
@@ -3,7 +3,7 @@ import { FaFacebookF, FaInstagram, FaTwitter, FaTiktok } from "react-icons/fa";
 
 export default function Footer() {
     return (
-        <footer className="bg-black text-white py-8 py-4 text-center mt-auto">
+        <footer className="bg-black text-white py-8 text-center mt-auto">
             <div className="container mx-auto px-6 md:px-12">
                 <div className="flex flex-col md:flex-row justify-between items-center gap-6">
 
